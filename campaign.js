de0c690dc1b354d5dea41a24d124f2b2c892081f
s15021ma <Mayu@net83-dhcp-73.sfc.keio.ac.jp>
2016-05-19 11:36:14 +0900

    20 代の独身女性には 2 番

--- campaign.js
+++ campaign.js
@@ -12,5 +12,7 @@ const CAMPAIGN_OTHERS = 4;
 function campaignNumber(age, sex, married){
     if(age >= 30 && married && sex == 1){
         return 1
+    }else if(20 <= age <= 29 && unmarried && sex == 0){
+        return 2
     }
 }
